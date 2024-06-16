154129e4d71e9a201deec1c4609092bf70b69d74
Abhishek Mishra <abhimishraop08@gmail.com>
2024-06-16 18:05:25 +0530

    fixed-stuff

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -103,7 +103,7 @@ const CartModal = () => {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
-              <span className="">${cart.subtotal.amount}</span>
+              {/* <span className="">${cart.selectedShippingOption.}</span> */}
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
               Shipping and taxes calculated at checkout.
