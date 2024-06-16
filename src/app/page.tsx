0cb70ca19eb49a79f50fb9fb1b8d4d01e70b8433
Abhishek Mishra <abhimishraop08@gmail.com>
2024-06-16 17:40:43 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+import CategoryList from "@/components/CategoryList";
+import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import Slider from "@/components/Slider";
+import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense, useContext, useEffect } from "react";
+
+const HomePage = async () => {
+
+  return (
+    <div className="">
+      <Slider />
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense>
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const ProductImages = ({ items }: { items: any }) => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <Image
+          src={items[index].image?.url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {items.map((item:any, i:number) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+            key={item._id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={item.image?.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
