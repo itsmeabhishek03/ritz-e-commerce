e38aa447e1e1615a68ffe1f07fab252241107df0
Abhishek Mishra <abhimishraop08@gmail.com>
2024-06-18 11:26:56 +0530

    fixed-2

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import Confetti from "react-confetti";
 
 const SuccessPage = () => {
@@ -22,14 +22,16 @@ const SuccessPage = () => {
   }, [orderId, router]);
 
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
+    <Suspense>
+      <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
       <Confetti width={2000} height={1000} />
-      <h1 className="text-6xl text-green-700">Successful</h1>
-      <h2 className="text-xl font-medium">
+       <h1 className="text-6xl text-green-700">Successful</h1>
+       <h2 className="text-xl font-medium">
         We sent the invoice to your e-mail
-      </h2>
+       </h2>
       <h3 className="">You are being redirected to the order page...</h3>
     </div>
+    </Suspense>
   );
 };
 
